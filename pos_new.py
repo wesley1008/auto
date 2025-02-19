61e1bcd0e67ad0f25404e1aa757b268966670880
Simon <yuxiangy87gmail.com>
2025-02-19 20:29:03 +0800

    posnew refresh

--- pos_new.py
+++ pos_new.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Imu
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from geometry_msgs.msg import PoseStamped as PS,TwistStamped as TW
 
 class PID:
     def __init__(self, kp, ki, kd):
@@ -24,8 +25,8 @@ class PID:
 
 class XYPositionController:
     def __init__(self, attitude_publisher):
-        self.vx_pid = PID(kp=0.01, ki=0.01, kd=0.02)  # X 方向速度 PID
-        self.vy_pid = PID(kp=1, ki=0.01, kd=0.02)  # Y 方向速度 PID
+        self.vx_pid = PID(kp=0.05, ki=0.0, kd=0.0)  # X 方向速度 PID
+        self.vy_pid = PID(kp=0.05, ki=0.0, kd=0.0)  # Y 方向速度 PID
         self.attitude_publisher = attitude_publisher
 
     def control_xy(self, vx, vy):
@@ -39,17 +40,14 @@ class XYPositionController:
 
         # 转换为四元数
         quat = R.from_euler('xyz', [roll_correction, pitch_correction, 0]).as_quat()
-
+        print(f"ox: {float(quat[0]):.10f}, oy: {float(quat[1]):.10f}, oz: {float(quat[2]):.10f}, ow: {float(quat[3]):.10f}")
+        
         # 发送姿态控制指令
         attitude_target = AttitudeTarget()
-        # attitude_target.orientation.x = quat[0]
-        # attitude_target.orientation.y = quat[1]
-        # attitude_target.orientation.z = quat[2]
-        # attitude_target.orientation.w = quat[3]
-        attitude_target.orientation.x = 0.0
-        attitude_target.orientation.y = 0.0
-        attitude_target.orientation.z = 0.0
-        attitude_target.orientation.w = 1.0
+        attitude_target.orientation.x = quat[0]
+        attitude_target.orientation.y = quat[1]
+        attitude_target.orientation.z = quat[2]
+        attitude_target.orientation.w = quat[3]
         attitude_target.type_mask = 7
         attitude_target.thrust = 0.5  # 保持当前高度
 
@@ -65,7 +63,10 @@ class UAVXYHoverNode(Node):
 
         self.attitude_publisher = self.create_publisher(AttitudeTarget, '/mavros/setpoint_raw/attitude', qos_profile)
         self.imu_subscription = self.create_subscription(Imu, '/mavros/imu/data', self.imu_callback, qos_profile)
+        self.pos_subscription = self.create_subscription(PS, '/mavros/local_position/pose', self.pose_callback, qos_profile)
+        self.vel_subscription = self.create_subscription(TW, '/mavros/local_position/velocity_local', self.vel_callback, qos_profile)
 
+        
         self.vx = 0.0
         self.vy = 0.0
         self.ax = 0.0
@@ -83,25 +84,28 @@ class UAVXYHoverNode(Node):
         current_time = imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec * 1e-9
         # current_time = imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec
         dt = current_time - self.last_time
-        if dt < 0.165:
-            return
-
-        # if self.last_time is None:
-        #     self.last_time = current_time
+        # if dt < 0.165:
         #     return
-
-        dt = current_time - self.last_time
 
         self.last_time = current_time
 
         self.vx += self.ax * dt
         self.vy += self.ay * dt
 
+    def pose_callback(self, msg):
+        ""
+
+    def vel_callback(self, msg):
+        ""
+        # self.vx = float(f"{msg.twist.linear.x:.4f}")
+        # self.vy = float(f"{msg.twist.linear.y:.4f}")
+
 
     def control_xy_hover(self):
         """ 计算 Pitch/Roll 角度，稳定 XY 位置 """
         self.xy_controller.control_xy(self.vx, self.vy)
-        self.get_logger().info(f"vx: {self.vx}, vy: {self.vy}, ax: {self.ax}, ay: {self.ay}")
+        # self.get_logger().info(f"vx: {self.vx}, vy: {self.vy}, ax: {self.ax}, ay: {self.ay}")
+        print(f"vx: {self.vx}, vy: {self.vy}")
 
 
 def main(args=None):
